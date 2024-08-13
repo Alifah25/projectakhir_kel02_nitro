800d65f9c7c7258f23a36f72745fa0e71919dece
Alifah25 <alifaqil2506@gmail.com>
2024-08-13 10:50:04 +0700

    new commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\NewProjekAkhir\ProjekAkhir\venv
+set VIRTUAL_ENV=D:\#MSIB\TUGAS AWAL\TUGAS 10\PROJECT01\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
